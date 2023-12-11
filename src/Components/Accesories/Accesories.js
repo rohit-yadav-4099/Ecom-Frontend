829ec9b0f3be941434497e9ff7088799780d09c4
Rohit Yadav <131008606+rohit-yadav-4099@users.noreply.github.com>
2023-12-11 08:03:51 +0530

    Update Accesories.js

--- src/Components/Accesories/Accesories.js
+++ src/Components/Accesories/Accesories.js
@@ -2,52 +2,56 @@ import useApi from "../../CustumHook/UseApi";
 
 
 const Accesories = () => {
-    const {data} = useApi()
+    const { data } = useApi()
     return (
         <div>
-             <h1 className="titleheading">Accesories</h1>
+            <h1 className="titleheading">Accesories</h1>
             <div className="n">
                 {data.filter((item) => item.id >= 39 && item.id <= 42).map(
                     (item, index) => {
                         return (
                             <div key={index} className="homecontainer">
                                 <div className="hoverimage">
-                                <div>
-                                    <img className="homeimage" src={item.image} alt="err"/>
+                                    <div>
+                                        <img className="homeimage" src={item.image} alt="err" />
+                                    </div>
+                                    <span className="title">{item.title}</span><br />
+                                    <span className="price">Rs.{item.price}.00</span><br />
+                                    <span >{item.rating}</span>
+                                    <div className="buynowAddcart">
+                                        <button className="buynow">Buy now</button>
+                                        <button className="addcart">Add cart</button>
+                                    </div>
                                 </div>
-                                <h1 className="title">{item.title}</h1>
-                                <h1 className="price">Rs.{item.price}</h1>
-                                <p >{item.rating}</p>
-                                <button className="addcart">Buy now</button>
-                                <button className="addcart">Add cart</button>
-                            </div>
                             </div>
                         )
 
                     })};
-               
-            </div><br/>
+
+            </div>
             <div className="n">
                 {data.filter((item) => item.id >= 43 && item.id <= 46).map(
                     (item, index) => {
                         return (
                             <div key={index} className="homecontainer">
                                 <div className="hoverimage">
-                                <div>
-                                    <img className="homeimage" src={item.image} alt="err"/>
-                                </div>
-                                <h1 className="title">{item.title}</h1>
-                                <h1 className="price">Rs.{item.price}</h1>
-                                <p >{item.rating}</p>
-                                <button className="addcart">Buy now</button>
-                                <button className="addcart">Add cart</button>
+                                    <div>
+                                        <img className="homeimage" src={item.image} alt="err" />
+                                    </div>
+                                    <span className="title">{item.title}</span><br />
+                                    <span className="price">Rs.{item.price}.00</span><br />
+                                    <span >{item.rating}</span>
+                                    <div className="buynowAddcart">
+                                        <button className="buynow">Buy now</button>
+                                        <button className="addcart">Add cart</button>
+                                    </div>
                                 </div>
                             </div>
                         )
 
                     })};
-               
-            </div><br/>
+
+            </div>
         </div>
     )
 }
